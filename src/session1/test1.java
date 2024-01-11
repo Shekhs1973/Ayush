a02140d0ca959a833e18ef5e52f83d1d6cd2d44c
Admin <Admin@DESKTOP-MTV198H>
2024-01-11 22:44:18 +0530

    Ayush Project Upload to GitHub

---
+++ src/session1/test1.java
@@ -0,0 +1,14 @@
+package session1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+	    for (int i=0 ; i<=10 ; i++) {
+	    	sum +=i;
+	    }
+	  
+	    System.out.println("Sum is "+sum);
+	}
+
+}
